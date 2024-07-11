180668121f44cf38668f867266adea12112539da
willfroq <w.delrosario@froq.nl>
2024-07-11 08:40:41 +0200

    Upload fixes

--- lib/AssetBundle/Switch/Action/ProcessSwitchUploadRequest.php
+++ lib/AssetBundle/Switch/Action/ProcessSwitchUploadRequest.php
@@ -4,28 +4,12 @@ declare(strict_types=1);
 
 namespace Froq\AssetBundle\Switch\Action;
 
-use Froq\AssetBundle\Switch\Enum\AssetResourceOrganizationFolderNames;
-use Froq\AssetBundle\Switch\ValueObject\CategoryFromPayload;
-use Froq\AssetBundle\Switch\ValueObject\PrinterFromPayload;
-use Froq\AssetBundle\Switch\ValueObject\ProductFromPayload;
-use Froq\AssetBundle\Switch\ValueObject\ProjectFromPayload;
-use Froq\AssetBundle\Switch\ValueObject\SupplierFromPayload;
-use Froq\AssetBundle\Switch\ValueObject\TagFromPayload;
-use Froq\AssetBundle\Utility\IsPathExists;
-use Froq\AssetBundle\Utility\IsTagExists;
 use Froq\PortalBundle\Api\ValueObject\ValidationError;
-use Pimcore\Model\DataObject\Organization;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ProcessSwitchUploadRequest
 {
-    public function __construct(
-        private readonly IsPathExists $isPathExists,
-        private readonly IsTagExists $isTagExists,
-    ) {
-    }
-
     /** @param array<int, ValidationError> $errors
      * @throws \Exception
      */
@@ -33,107 +17,6 @@ final class ProcessSwitchUploadRequest
     {
         if (!($file instanceof UploadedFile)) {
             $errors[] = new ValidationError(propertyPath: 'fileContents', message: sprintf('FileContents %s is not a file.', $file));
-        }
-
-        $organization = Organization::getByCode((string) $request->request->get('customerCode'))->current(); /** @phpstan-ignore-line */
-        if (!($organization instanceof Organization)) {
-            return;
-        }
-
-        $rootAssetResourceFolder = $organization->getObjectFolder() . '/';
-
-        $productData = (array) json_decode((string) $request->request->get('productData'), true);
-        $categories = $productData['productCategories'] ?? null ;
-        $product = new ProductFromPayload(
-            productName: $productData['productName'] ?? null,
-            productEAN: $productData['productEAN'] ?? null,
-            productSKU: $productData['productSKU'] ?? null,
-            productAttributes: [],
-            productNetContentStatement: $productData['productNetContentStatement'] ?? null,
-            productNetContents: [],
-            productNetUnitContents: [],
-            productCategories: new CategoryFromPayload(
-                brand: $categories['brand'] ?? null,
-                campaign: $categories['campaign'] ?? null,
-                market: $categories['market'] ?? null,
-                segment: $categories['segment'] ?? null,
-                platform: $categories['platform'] ?? null,
-            )
-        );
-
-        $productKey = (string) $product->productName;
-        $productPath = $rootAssetResourceFolder.AssetResourceOrganizationFolderNames::Products->readable().'/';
-
-        if (!isset($product->productName)) {
-            $errors[] = new ValidationError(propertyPath: 'Product', message: sprintf('Product %s has no product name.', $productPath.$productKey));
-        }
-
-        if (($this->isPathExists)($productKey, $productPath)) {
-            $errors[] = new ValidationError(propertyPath: 'Product', message: sprintf('Product %s path already exists, this has to be unique.', $productPath.$productKey));
-        }
-
-        $tagData = (array) json_decode((string) $request->request->get('tagData'), true);
-        $tagPath = $rootAssetResourceFolder.AssetResourceOrganizationFolderNames::Tags->name.'/';
-
-        foreach ($tagData as $tagDatum) {
-            $tag = new TagFromPayload(code: $tagDatum['code'] ?? null, name: $tagDatum['name'] ?? null);
-
-            if (($this->isPathExists)((string) $tag->code, $tagPath)) {
-                $errors[] = new ValidationError(propertyPath: 'Tag', message: sprintf('Tag %s path already exists, this has to be unique.', $tagPath.$tag->code));
-            }
-
-            if (($this->isTagExists)('Code', (string) $tag->code)) {
-                $errors[] = new ValidationError(propertyPath: 'Tag', message: sprintf('Tag %s already exists, this has to be unique.', $tag->code));
-            }
-        }
-
-        $projectData = (array) json_decode((string) $request->request->get('projectData'), true);
-        $project = new ProjectFromPayload(
-            projectCode: $projectData['projectCode'] ?? null,
-            projectName: $projectData['projectName'] ?? null,
-            pimProjectNumber: $projectData['pimProjectNumber'] ?? null,
-            froqProjectNumber: $projectData['froqProjectNumber'] ?? null,
-            customerProjectNumber: $projectData['customerProjectNumber'] ?? null,
-            froqName: $projectData['froqName'] ?? null,
-            description: $projectData['description'] ?? null,
-            projectType: $projectData['projectType'] ?? null,
-            status: $projectData['status'] ?? null,
-            location: $projectData['location'] ?? null,
-            deliveryType: $projectData['deliveryType'] ?? null,
-        );
-
-        $printerData = (array) json_decode((string) $request->request->get('printerData'), true);
-        $printer = new PrinterFromPayload(
-            printingProcess: $printerData['printingProcess'] ?? null,
-            printingWorkflow: $printerData['printingWorkflow'] ?? null,
-            epsonMaterial: $printerData['epsonMaterial'] ?? null,
-            substrateMaterial: $printerData['substrateMaterial'] ?? null,
-        );
-
-        $printerPath = $rootAssetResourceFolder.AssetResourceOrganizationFolderNames::Printers->name.'/';
-        $printerKey = $printer->printingProcess;
-
-        if (($this->isPathExists)((string) $printerKey, $printerPath)) {
-            $errors[] = new ValidationError(propertyPath: 'Printer', message: sprintf('Printer %s path already exists, this has to be unique.', $printerPath.$printerKey));
-        }
-
-        $supplierData = (array) json_decode((string) $request->request->get('supplierData'), true);
-        $supplier = new SupplierFromPayload(
-            supplierCode: $supplierData['supplierCode'] ?? null,
-            supplierCompany: $supplierData['supplierCompany'] ?? null,
-            supplierContact: $supplierData['supplierContact'] ?? null,
-            supplierStreetName: $supplierData['supplierStreetName'] ?? null,
-            supplierStreetNumber: $supplierData['supplierStreetNumber'] ?? null,
-            supplierPostalCode: $supplierData['supplierPostalCode'] ?? null,
-            supplierPhoneNumber: $supplierData['supplierPhoneNumber'] ?? null,
-            supplierEmail: $supplierData['supplierEmail'] ?? null,
-        );
-
-        $supplierPath = $rootAssetResourceFolder.AssetResourceOrganizationFolderNames::Suppliers->name.'/';
-        $supplierKey = $supplier->supplierCode;
-
-        if (($this->isPathExists)((string) $supplierKey, $supplierPath)) {
-            $errors[] = new ValidationError(propertyPath: 'Supplier', message: sprintf('Supplier %s path already exists, this has to be unique.', $supplierPath.$supplierKey));
         }
     }
 }
