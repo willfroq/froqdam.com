f2e51da4e3c3ac2b83d06fb14b47337b8996ae2a
willfroq <w.delrosario@froq.nl>
2025-04-09 16:01:05 +0200

    07-04-2025 PRS

--- var/classes/definition_ApiToken.php
+++ var/classes/definition_ApiToken.php
@@ -17,7 +17,7 @@ return \Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
    'name' => 'ApiToken',
    'description' => '',
    'creationDate' => 0,
-   'modificationDate' => 1744138957,
+   'modificationDate' => 1744128817,
    'userOwner' => 2,
    'userModification' => 2,
    'parentClass' => '',
